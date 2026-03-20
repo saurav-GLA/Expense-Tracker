5a9f6b8f3fb960704cbebbaf44e6209d08b0b7f0
saurav-GLA <saurav.rai@gla.ac.in>
2026-03-20 09:52:28 +0530

    first commit

---
+++ backend/controllers/authController.js
@@ -0,0 +1,72 @@
+const User = require("../models/User")
+const  jwt = require("jsonwebtoken");
+
+// Generate JWT token
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: "1h"});
+};
+
+// Register User
+exports.registerUser = async (req, res) => {
+    // console.log("BODY:", req.body);
+    const {fullName, email, password, profileImageUrl } = req.body;
+
+    //Validation: Check for missing fields
+    if(!fullName || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
+    try {
+        //Check if email already exists
+        const existingUser = await User.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({message: "Email already in use" });
+        }
+
+        //Create the user
+        console.log("About to create user with:", { fullName, email, profileImageUrl });
+        const user = await User.create({
+            fullName,
+            email, 
+            password, 
+            profileImageUrl,
+        });
+
+        res.status(201).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        })
+    } catch (err) {
+       res
+         .status(500)
+         .json({message : "Error registering user", erro: err.message});
+    };
+};
+
+// Login User
+exports.loginUser = async (req, res) => {
+        const { email, password } = req.body;
+    if(!email || !password ) {
+        return res.status(400).json({ message: "All feilds are required" });
+    }
+    try{
+        const user = await User.findOne({ email });
+        if(!user || !(await user.comparePassword(password))) {
+            return res.status(400).json({message: "Invalid credentials" });
+        }
+
+        res.status(200).json({
+            id: user._id,
+            user,
+            token: generateToken(user._id),
+        });
+    }catch (err){
+        res
+         .status(500)
+         .json({message : "Error logging in", erro: err.message});
+    }
+}
+
+// Register User
+exports.getUserInfo = async (req, res) => {}
