b47a666268b7997013e780a51bced4c1201fe40b
saurav-GLA <saurav.rai@gla.ac.in>
2026-03-25 10:17:14 +0530

    Added profile Image and CharAvatar

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 import DashboardLayout from '../../components/layout/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useEffect } from 'react';
 
 const Home = () => {
+    useUserAuth();
+
+    const navigate = useNavigate();
+
+    const[dashboardData, setDashboardData] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const fetchDashboardData = async () => {
+        try {
+            const response = await axiosInstance.get(
+                `${API_PATHS.DASHBOARD.GET_DATA}`
+            );
+
+            if (response.data) {
+                setDashboardData(response.data);
+            }
+        } catch (error) {
+            console.log("Something went wrong. Please try again.", error);
+        }finally {
+            setLoading(false);
+        };
+    };
+    useEffect(() => {
+        fetchDashboardData();
+        return () => {};
+    }, []);
+
     return(
         <DashboardLayout activeMenu="Dashboard">
             <div className="my-5 mx-auto">
