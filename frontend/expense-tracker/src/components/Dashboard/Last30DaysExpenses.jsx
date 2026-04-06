9dd17b8aa5292dd057216dd95b7b63833f8235cd
Lalit Chauhan <lkc92055@gmail.com>
2026-04-06 13:01:14 +0530

    Add Last30DaysExpenses component

---
+++ frontend/expense-tracker/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Last30DaysExpenses = ({data}) => {
+  
+  const [chartData, setChartData]
+  return (
+  <div className="card col-span-1">
+    <div className="flex items-canter justify-between">
+      <h5 className="text-lg"> Last 30 Days Expenses</h5>
+    </div>
+
+    
+  </div>
+  )
+}//2:55
+export default Last30DaysExpenses
