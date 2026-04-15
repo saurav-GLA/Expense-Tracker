70decb1d0bc863cb40913d4ce111c2e6ce04e1d5
saurav-GLA <saurav.rai@gla.ac.in>
2026-04-15 14:09:30 +0530

    added bar chart in Income Page

--- frontend/expense-tracker/src/pages/Dashboard/Income.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Income.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import DashboardLayout from '../../components/layout/DashboardLayout'
 import { useState } from 'react'
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useEffect } from 'react';
 
 const Income = () => {
 
@@ -10,8 +13,45 @@ const Income = () => {
         show: false,
         data: null,
     });
-
     const [openAddIncomeModal, setOpenAddIncomeModel] = useState(second);
+
+
+    // Get All Income Details
+    const fetchIncomeDetails = async () => {
+        if(loading) return;
+
+        setLoading(true);
+        try {
+            const response = await axiosInstance.get(
+                `${API_PATHS.INCOME.GET_ALL_INCOME}`
+            );
+            if (response.data) {
+                setIncomeData(response.data);
+            }
+        } catch (error) {
+            console.log("Something went wrong. Please try again.", error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    // Handle Add Income
+    const handleAddIncome = async (income) => {};
+
+    // Delete Income
+    const deleteIncome = async (id) => {};
+
+    // handle download income details
+    const handleDownloadIncomeDetails = async () => {};
+
+    useEffect(() =>{
+        fetchIncomeDetails()
+        return () => {
+            second
+        }
+    }, [third])
+
+    //
     return(
         <DashboardLayout activeMenu="Income">
             <div className="my-5 mx-auto">
